85006d2b141f425aea910d6b9ddecfd56a2280dc
Cahyo Adi Prasetia <cahyoadiprasetia@gmail.com>
2024-03-07 23:48:20 +0700

    Implement eloquent erm

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        //tambah data user dengan eloquent model
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data); //tambahkan data ke table m_user
+        
+        //coba akses UserModel
+        $user = UserModel::all(); //ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
+    }
+}
