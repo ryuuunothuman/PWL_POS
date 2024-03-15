dd30f96b4fab59dd0e1b96ac16c0a9c4a1c42e86
Cahyo Adi Prasetia <cahyoadiprasetia@gmail.com>
2024-03-15 22:24:52 +0700

    feat:implement Relationship

---
+++ app/Models/LevelModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class LevelModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'm_level';    //mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'level_id'; //mendefinisikan primary key dari tabel yang digunakan
+
+    public function user(): HasMany
+    {
+        return $this->hasMany(UserModel::class, 'level_id', 'level_id');
+    }
+}
