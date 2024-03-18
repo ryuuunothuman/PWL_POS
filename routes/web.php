a77a8d644bb850a3feefa7912bc6ef8cc671995d
Cahyo Adi Prasetia <cahyoadiprasetia@gmail.com>
2024-03-18 23:44:01 +0700

    feat:implement Integrasi Laravel dengan AdminLTE 3 Praktikkum 1

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'hehe';
+    return view('welcome');
 });
 
 Route::get('/level', [LevelController::class, 'index']);
