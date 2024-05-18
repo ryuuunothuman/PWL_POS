acfafb53cccdb8bdba0f64c8205a727838894f27
Cahyo Adi Prasetia <cahyoadiprasetia@gmail.com>
2024-05-18 23:06:58 +0700

    feat(JS-10 Praktikkum 4): CRUD implementation in RESTful API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LevelController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
 use App\Http\Controllers\Api\RegisterController;
@@ -28,3 +29,9 @@ Route::middleware('auth:api')->get('/user', function(Request $request) {
 });
 
 Route::post('/logout', LogoutController::class)->name('logout');
+
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
